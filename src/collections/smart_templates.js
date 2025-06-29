e591a2b733a130fb178df9eace83337cce490885
Brian Joseph Petro <brian@smartconnections.app>
2025-06-29 11:01:28 -0400

    refactor: Update SmartTemplates to extend Collection directly, simplifying inheritance

--- src/collections/smart_templates.js
+++ src/collections/smart_templates.js
@@ -7,15 +7,15 @@
  * Also merges the chat_model's settings_config if present, so that UI can handle them together.
  */
 
-import { SmartTemplates as BaseSmartTemplates } from "smart-templates-obsidian/src/collections/smart_templates.js";
+import { Collection } from "smart-collections";
 import { AjsonSingleFileCollectionDataAdapter } from "smart-collections/adapters/ajson_single_file.js";
-import { SmartTemplate } from "smart-templates-obsidian/src/items/smart_template.js";
+import { SmartTemplate } from "../items/smart_template.js";
 
 /**
  * @class SmartTemplates
- * @extends BaseSmartTemplates
+ * @extends Collection
  */
-export class SmartTemplates extends BaseSmartTemplates {
+export class SmartTemplates extends Collection {
 
   get settings_config() {
     return {
